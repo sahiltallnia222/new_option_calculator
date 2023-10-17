63614227cb5bf7dd4fa7b8884f880fba0a29422b
SahilTallnia <sahiltallnia222@gmail.com>
2023-10-18 03:46:57 +0530

    upload

--- webapp/settings.py
+++ webapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'agrq4q124q354y25w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.azurewebsites.net']
 
